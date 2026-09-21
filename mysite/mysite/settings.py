8c66f6834e270bd9f52e05bdc95ea8535bc106e4
Nurbol9 <nurbol05.0909@gmail.com>
2026-09-21 23:15:01 +0600

    new file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lalafo_ii33',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'adminadmin',
-        'HOST': 'localhost',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
         'PORT': 5432
     }
 }
